9d5db74543ab7f98da19827c1c4597503f199be3
Sebas LG <neoranga55@gmail.com>
2015-12-05 00:16:42 +0100

    Add RX console hello world

--- app/src/main/java/com/neoranga55/rxdemo/MainActivity.java
+++ app/src/main/java/com/neoranga55/rxdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.neoranga55.rxdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> sub) {
+                        sub.onNext("Hello, world!");
+                        sub.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) { System.out.println(s); }
+
+            @Override
+            public void onCompleted() { }
+
+            @Override
+            public void onError(Throwable e) { }
+        };
+
+        myObservable.subscribe(mySubscriber);
+
+        Observable<String> myShortObservable = Observable.just("Short hello, world!");
+        Action1<String> onNextAction = new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println(s);
+            }
+        };
+        myShortObservable.subscribe(onNextAction);
     }
 }
