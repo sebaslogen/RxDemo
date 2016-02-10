63169ee4a276e06faa7025ce4750fa499ce140f8
Sebas LG <neoranga55@gmail.com>
2016-02-10 22:00:50 +0100

    Add PublishSubject example to do async text search

--- app/src/main/java/com/neoranga55/rxdemo/MainActivity.java
+++ app/src/main/java/com/neoranga55/rxdemo/MainActivity.java
@@ -2,9 +2,14 @@ package com.neoranga55.rxdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Subscriber;
@@ -13,6 +18,7 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.exceptions.Exceptions;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 import rx.subscriptions.CompositeSubscription;
 
 @SuppressWarnings("SameReturnValue")
@@ -78,6 +84,46 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(System.out::println);
         mSubscriptions.add(subs4);
+
+        // More advanced RxJava (including Subjects)
+        final EditText inputField = (EditText) findViewById(R.id.editText);
+        final TextView resultField = (TextView) findViewById(R.id.textView);
+        final PublishSubject searchSubject = PublishSubject.create();
+        searchSubject
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .observeOn(Schedulers.io()) // Network call should be on another thread, not on UI thread
+                .map(s -> {
+                    try { // Simulate network call
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return s;
+                })
+                .observeOn(AndroidSchedulers.mainThread()) // Response needs to be on UI thread
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String res) {
+                        resultField.setText(res);
+                    }
+                });
+
+        inputField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchSubject.onNext(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private String deferExceptionDemo() {
